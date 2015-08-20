bb59f4a437e3bebca0d7cc7fb970e6fd5208707a
wenlai <wenchenglai@gmail.com>
2015-08-20 14:09:01 -0400

    minor update

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
 using System.Web.Security;
-using System.Web.UI.WebControls;
 using System.Data;
 
 public partial class Login : System.Web.UI.Page
